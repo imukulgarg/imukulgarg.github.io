9945eeddaffa3ecca2bf84ccd9c01e0f88ea2199
imukulgarg <75804324+imukulgarg@users.noreply.github.com>
2020-12-16 14:37:02 +0530

    updated!

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const popup = document.querySelector('.popup_container');
 const internal_popup = document.querySelector('.popup');
 const btn = document.querySelector('.popup-btn');
 const close = document.querySelector('.close');
+const burger = document.querySelector('.button');
+const nav_list = document.querySelector('.nav_list');
+const links = document.querySelectorAll('.nav_list li');
+const icon = document.querySelector('.button i');
 
 window.addEventListener('DOMContentLoaded', () => {
     const text = "Full Stack Developer in Making!!      ";
@@ -27,3 +31,15 @@ close.addEventListener('click', () => {
     internal_popup.classList.toggle('popup_animate');
 })
 
+burger.addEventListener('click', change)
+
+links.forEach(el => {
+    el.addEventListener('click', change)
+})
+
+function change() {
+    nav_list.classList.toggle('nav-active');
+    burger.classList.toggle('button-active');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-times');
+}
